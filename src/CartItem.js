8ba3ec0fac85c9c61dc5917872ba2d8dbc7ce1cc
pahujajayesh <jayesh.pahuja123@gamil.com>
2021-09-27 17:22:53 +0530

    Added Constructor and buttons for class action-icons

---
+++ src/CartItem.js
@@ -0,0 +1,42 @@
+import React from "react";
+class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            title:'Mobile Phone',
+            price:999,
+            qty:1,
+            img:''
+        }
+    }
+    render() {
+        const{title,price,qty}=this.state
+        return (
+            <div className="cary-item">
+                <div className="left-block">
+                    <img alt="" style={styles.image} />
+                </div>
+                <div className="right-block">
+                    <div style={{ fontSize: 35, color: 'red' }}>{title}</div>
+                    <div style={{ color: 'blue' }}>Price:Rs{price}</div>
+                    <div style={{ color: 'darkgray' }}>Qty:{qty}</div>
+                    <div className="action-icons">
+                        <img alt="minus"className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"></img>
+                        <img alt="plus"className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"></img>
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"></img>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+const styles = {
+    image: {
+        height: 100,
+        width: 100,
+        borderRadius: 4,
+        background: '#ccc'
+    }
+}
+
+export default CartItem;
